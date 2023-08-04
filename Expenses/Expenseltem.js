d3a732028a4e9263cd30f6ae35e760916ae8c29f
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-04 23:43:07 +0530

    Update Expenseltem.js

--- Expenses/Expenseltem.js
+++ Expenses/Expenseltem.js
@@ -1,24 +1,20 @@
-import React,{useState} from 'react';
+import React from 'react';
 import ExpenseDate from "./ExpenseDate.js";
 import Card from "../UI/Card.js";
 // import Expensewa from "./ExpenseDetails.js";
 import "./Expense.css";
 
 const Expense = (props)=>{
-    const[amount,setTitle] =   useState(props.amount);
 
-    const clickHandler = ()=>{
-        setTitle('$100');
-    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             {/* <Expensewa amount={props.amount} title={props.title} /> */}
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
-                <div className="expense-item__price">{amount}</div>
+                <div className="expense-item__price">₹{props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>          
+                   
        </Card>
     );
     
