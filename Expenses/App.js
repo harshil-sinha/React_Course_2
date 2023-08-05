493bedbf8912e19377c91164fef0e3b5f028155a
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-05 11:46:48 +0530

    Update App.js

--- Expenses/App.js
+++ Expenses/App.js
@@ -3,39 +3,40 @@
 
 // import ExpenseItem from "./components/Expenseltem.js";
 import Expenses from "./components/Expenses/Expenses.js";
-import React from 'react';
+import React,{useState} from 'react';
 import NewExpense from "./components/NewExpense/NewExpense.js";
 
+const DUMMY_EXPENSES =[{
+  id: 'e1',
+  title: 'Toilet Paper',
+  amount: 94.12,
+  date: new Date(2020, 7, 14)
+
+},
+{
+  id: 'e2',
+  title: 'New Tv',
+  amount: 800.67,
+  date: new Date(2021, 2, 11)
+
+},
+{
+  id: 'e3',
+  title: 'Car Insurance',
+  amount: 294.67,
+  date: new Date(2021, 2, 15)
+
+},
+{
+  id: 'e4',
+  title: 'New Desk(Wooden)',
+  amount: 450,
+  date: new Date(2021, 7, 13)
+
+}
+];
 const App = ()=> {
-  const expenses = [{
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14)
-
-    },
-    {
-      id: 'e2',
-      title: 'New Tv',
-      amount: 800.67,
-      date: new Date(2021, 2, 11)
-
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 15)
-
-    },
-    {
-      id: 'e4',
-      title: 'New Desk(Wooden)',
-      amount: 450,
-      date: new Date(2021, 7, 13)
-
-    }
-  ];
+  
   // return React.createElement(
   //   'div', 
   //    {},
@@ -44,10 +45,12 @@ const App = ()=> {
     
     
   //   );
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = expense =>{
-    console.log("In App.js");
-    console.log(expense);
-  }
+    setExpenses(prevExpenses=>{
+      return [expense,...prevExpenses];
+    });
+  };
   return (
     <div className="App">
       <NewExpense onAddExpense={addExpenseHandler}/>
