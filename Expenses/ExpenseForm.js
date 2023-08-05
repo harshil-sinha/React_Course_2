eb34c01cf5e4d549a7653dac3e18b398fdd308bc
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-05 17:17:49 +0530

    Update ExpenseForm.js

--- Expenses/ExpenseForm.js
+++ Expenses/ExpenseForm.js
@@ -63,9 +63,10 @@ const ExpenseForm = (props)=>{
                 <input type="date" value={enteredDate}min="2019-01-01" max="2022-12-31"  onChange={dateChangeHandler}/>
             </div>
         </div>
-        <div className="new-expense__actios">
-            <button type="submit">Add Expense</button>
-        </div>
+        <div className='new-expense__actions'>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
+        <button type='submit'>Add Expense</button>
+      </div>
     </form>
     )
 }
